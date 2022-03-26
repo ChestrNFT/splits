07c37c17bedca676ec689e9fdaf5e70528b65bfe
Sofiane <sofiane.ouafir@live.fr>
2022-03-26 19:13:29 -0400

    task - updated vault created event

--- contracts/mock/MockCollection.sol
+++ contracts/mock/MockCollection.sol
@@ -2,14 +2,15 @@
 pragma solidity ^0.8.4;
 
 import {ICoreCollection} from "../../interfaces/ICoreCollection.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MockCollection is ICoreCollection {
+contract MockCollection is Ownable {
     address royaltyVault;
 
     /**
      * @dev Set the address of the RoyaltyVaultProxy contract
      */
-    function setRoyaltyVault(address _royaltyVault) external override {
+    function setRoyaltyVault(address _royaltyVault) external {
         royaltyVault = _royaltyVault;
     }
 }
