c766e16e2f62990301fa579e065f120df16056ac
Sofiane <sofiane.ouafir@live.fr>
2022-03-28 19:36:54 -0400

    task - removed unused import from splitter

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.4;
 import {SplitStorage} from "./SplitStorage.sol";
 import {IRoyaltyVault} from "@chestrnft/royalty-vault/interfaces/IRoyaltyVault.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import {IWETH} from "../interfaces/IWETH.sol";
 
 /**
  * @title Splitter
